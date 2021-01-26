fd087cc12a92b3b5dc3369fe2a61a5f10b670635
jabir-dev <61085403+jabir-dev@users.noreply.github.com>
2021-01-26 08:46:54 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+from tokens import cmc_token
+from flask import Flask
+from flask import request
+from flask import Response
+import requests,json,re
+
+from flask_sslify import SSLify
+
+token = 'X' #This is X your Telegram Bot id
+
+app = Flask(__name__)
+sslify = SSLify(app)
+
+
+def write_json(data, filname='response.json'):
+	with open(filname, 'w') as f:
+		json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+
+def get_cmc_data(crypto):
+	url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+	params = {'symbol': crypto, 'convert': 'USD'}
+	headers = {'X-CMC_PRO_API_KEY': cmc_token}
+
+	r = requests.get(url, headers=headers, params=params).json()
+
+	price = r['data'][crypto]['quote']['USD']['price']
+
+	return price
+	
+
+def parse_message(message):
+	chat_id = message['message']['chat']['id']
+	txt = message['message']['text']     #/btc or /maid
+
+	pattern = r'/[a-zA-Z]{2,4}'
+
+	ticker = re.findall(pattern, txt) 
+
+	if ticker:
+		symbol = ticker[0][1:].upper() # /btc > btc  .strip('/) 
+	else:
+		symbol = ''
+	
+	return chat_id, symbol
+
+
+def send_message(chat_id, text='bla-bla-bla'):
+	url = f'https://api.telegram.org/bot{token}/sendMessage'
+	payload = {'chat_id': chat_id, 'text': text}
+
+	r = requests.post(url, json=payload)
+	return r
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+	if request.method == 'POST':
+		msg = request.get_json()
+		chat_id, symbol = parse_message(msg)
+
+		if not symbol:
+			send_message(chat_id, 'Wrong data')
+			return Response('Ok', status=200)
+
+		price = get_cmc_data(symbol)
+		send_message(chat_id, price)
+		#write_json(msg, 'telegram_request.json')
+
+		return Response('Ok', status=200)
+	else:
+		return '<h1>CoinMarketCap Bot</h1>'
+
+
+
+def main():
+	print(get_cmc_data('BTC'))
+
+
+
+
+# https://api.telegram.org/bot X/getMe
+
+# https://api.telegram.org/bot X /sendMessage?chat_id=651267590&text=Hello user
+
+# https://api.telegram.org/bot X /setWebhook?url=https://aqacas.pythonanywhere.com/
+
+
+
+
+if __name__ == '__main__':
+	#main()
+	app.run(debug=True)
